61b2306a761f9a4ad8263e92683c3ddffbd05921
Pakillo <f.rodriguez.sanc@gmail.com>
2019-02-04 19:36:52 +0100

    fix hashtags at beginning of tweet

--- R/print_tweet.R
+++ R/print_tweet.R
@@ -1,6 +1,8 @@
 print_tweet <- function(df) {
   if (nrow(df) > 1) df <- df[1, ]
   df$text <- gsub("\n+#", " #", df$text)
+  df$text <- gsub("^#", " #", df$text)
+  df$text <- gsub("^ #", " #", df$text)
   cat(df$text, "\n\n")
   cat(df$name, " (@", df$screen_name, ")", "\n\n", sep = "")
   cat("[", format(df$created_at, "%d-%m-%Y"), "](", df$status_url, ")\n", sep = "")
