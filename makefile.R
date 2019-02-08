c1b34e1790fed38ed52971127eaa1c59ad38ad55
Pakillo <f.rodriguez.sanc@gmail.com>
2019-02-08 23:41:58 +0100

    add dataset with retweets

--- makefile.R
+++ makefile.R
@@ -2,3 +2,12 @@ system("Rscript gathertweet.R search '#sibecol2019 OR #sibecol19'")  # get new t
 system("Rscript gathertweet.R update")   # update tweets data
 
 rmarkdown::render(input = "index.Rmd")
+
+
+#######################################
+
+### including retweets ###
+
+system("Rscript gathertweet.R search --file=retweets.rds --include_rts '#sibecol2019 OR #sibecol19'")  # get new tweets
+system("Rscript gathertweet.R update --file=retweets.rds")   # update tweets data
+
